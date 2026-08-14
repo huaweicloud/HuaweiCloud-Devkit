8f1d96803546e69d268f011754db71641759b10b
lipeixin <lipeixin9999@126.com>
2026-08-14 19:39:25 +0800

    fix(sandbox): restore official hdkitservice endpoint URL

--- plugins/huaweicloud-core/src/sandbox/hdkitservice-api.mjs
+++ plugins/huaweicloud-core/src/sandbox/hdkitservice-api.mjs
@@ -2,7 +2,7 @@ import { getCredentials } from './hwlink-api.mjs';
 
 const HDKIT_BASE_URL =
   process.env.HDKITSERVICE_ENDPOINT ||
-  'http://devkit.topxtopx.com/rest/developer/server/hdkitservice/';
+  'https://devkit.huaweicloud.com/rest/developer/server/hdkitservice/';
 
 async function hdkitRequest(method, path, body, timeoutMs = 300000) {
   const { ak, sk, securitytoken } = getCredentials();
